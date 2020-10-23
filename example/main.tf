b82074236cd0316b70789416e837bd10f1a3898b
mflinn-broad <mflinn@broadinstitute.org>
2020-10-23 15:06:30 -0400

    update tf module readme

--- example/main.tf
+++ example/main.tf
@@ -1,3 +1,18 @@
+/**
+ * # Terratest Demo Workflow module
+ *
+ * This is a simple terra form module which create a GCE VM instance and GCS bucket. 
+ * It is intended to be used just for a demonstration of a sample automated infrasture 
+ * testing workflow using [terratest](https://terratest.gruntwork.io/)
+ *
+ * To break the automated tests make a pr with the instance or bucket name hard coded
+ * rather than using a variable
+ *
+ * This documentation is generated with [terraform-docs](https://github.com/segmentio/terraform-docs)
+ * `terraform-docs markdown --no-sort . > README.md`
+ */
+
+
 resource "google_compute_instance" "test_vm" {
   project      = var.google_project
   name         = var.instance_name
