a115bfc0e2a6e9969421493ed2b96d3c90470665
mflinn-broad <mflinn@broadinstitute.org>
2021-03-24 16:53:44 -0400

    massively simplify tests

--- test/example_test.go
+++ test/example_test.go
@@ -4,11 +4,6 @@ import (
 	"fmt"
 	"strings"
 	"testing"
-	"time"
-
-	"github.com/gruntwork-io/terratest/modules/ssh"
-
-	"github.com/gruntwork-io/terratest/modules/retry"
 
 	"github.com/gruntwork-io/terratest/modules/gcp"
 	"github.com/gruntwork-io/terratest/modules/random"
@@ -50,98 +45,4 @@ func TestTerraformExample(t *testing.T) {
 
 	// attempt running init and  apply, will fail if errors
 	terraform.InitAndApply(t, terraformOptions)
-
-	// get output values
-	instanceName := terraform.Output(t, terraformOptions, "instance_name")
-
-	// assert storage bucket exists
-	gcp.AssertStorageBucketExists(t, expectedBucketName)
-
-	// Add a tag to instance
-	instance := gcp.FetchInstance(t, projectId, instanceName)
-	instance.SetLabels(t, map[string]string{"testing": "testing-tag-value2"})
-
-	// check labels with retry
-	maxRetries := 12
-	timeBetweenRetries := 5 * time.Second
-	expectedText := "testing-tag-value2"
-
-	// Check if the instance has the expected tag
-	retry.DoWithRetry(t, fmt.Sprintf("Checking Instance %s for labels", instanceName), maxRetries, timeBetweenRetries, func() (string, error) {
-		// look up the tags for an instance
-		instance := gcp.FetchInstance(t, projectId, instanceName)
-		instanceLabels := instance.GetLabels(t)
-
-		testingTag, ok := instanceLabels["testing"]
-		actualText := strings.TrimSpace(testingTag)
-		if !ok {
-			return "", fmt.Errorf("Expected the tag 'testing' to exits")
-		}
-
-		if actualText != expectedText {
-			return "", fmt.Errorf("Expected GetLabelsForComputeInstanceE to return '%s' but got '%s'", expectedText, actualText)
-		}
-		return "", nil
-	})
-}
-
-func TestSSHAccess(t *testing.T) {
-	t.Parallel()
-
-	exampleDir := test_structure.CopyTerraformFolderToTemp(t, "../", "example")
-
-	projectID := gcp.GetGoogleProjectIDFromEnvVar(t)
-	randomValidGcpName := gcp.RandomValidGcpName()
-
-	// according to docs framework has issue with asia-east2 so exclude it
-	zone := gcp.GetRandomZone(t, projectID, nil, nil, []string{"asia-east2"})
-
-	terraformOptions := &terraform.Options{
-		TerraformDir: exampleDir,
-
-		// tfvars
-		Vars: map[string]interface{}{
-			"google_project": projectID,
-			"instance_name":  randomValidGcpName,
-			"bucket_name":    randomValidGcpName,
-			"zone":           zone,
-		},
-	}
-
-	defer terraform.Destroy(t, terraformOptions)
-
-	terraform.InitAndApply(t, terraformOptions)
-
-	// get pubIp for test instance
-	publicIp := terraform.Output(t, terraformOptions, "public_ip")
-
-	instance := gcp.FetchInstance(t, projectID, randomValidGcpName)
-
-	testMessage := "Hello World"
-	sshUsername := "terratest"
-
-	keyPair := ssh.GenerateRSAKeyPair(t, 2048)
-	instance.AddSshKey(t, sshUsername, keyPair.PublicKey)
-
-	host := ssh.Host{
-		Hostname:    publicIp,
-		SshKeyPair:  keyPair,
-		SshUserName: sshUsername,
-	}
-
-	maxRetries := 20
-	sleepBetweenRetries := 3 * time.Second
-
-	retry.DoWithRetry(t, "Attempting to SSH", maxRetries, sleepBetweenRetries, func() (string, error) {
-		output, err := ssh.CheckSshCommandE(t, host, fmt.Sprintf("echo '%s'", testMessage))
-		if err != nil {
-			return "", err
-		}
-
-		if strings.TrimSpace(testMessage) != strings.TrimSpace(output) {
-			return "", fmt.Errorf("Expected: %s. Got: %s\n", testMessage, output)
-		}
-
-		return "", nil
-	})
 }
