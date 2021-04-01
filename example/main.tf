4d0a4a5f4c9d33cfd8fd9a617f42af2bc0f2f6db
aliceajatwit <56600589+aliceajatwit@users.noreply.github.com>
2021-04-01 15:15:43 -0400

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -36,6 +36,8 @@ resource "google_storage_bucket" "test_bucket" {
   project  = var.google_project
   name     = var.bucket_name
   location = var.bucket_location
+  uniform_bucket_level_access = true
+
 }
 
 terraform {
